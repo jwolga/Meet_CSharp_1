4de329c9bf70b72d711fc90f01fb0fcd0d397c34
OS <jwolga@yandex.ru>
2022-08-28 23:32:12 +0300

    added new project Example003

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+int result = numberA + numberB;
+Console.WriteLine($"The first number {numberA}, the second number {numberB} and sum {result}");
