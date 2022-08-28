a6db5c7034e36fc5bddea71522baaa8e2133892c
OS <jwolga@yandex.ru>
2022-08-28 23:18:32 +0300

    added new file gitignore and new project Example002

---
+++ Example002_НelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Hello, ");
+Console.WriteLine(username);
